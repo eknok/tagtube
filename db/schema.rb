95f598d2be6c9a6a984f12d827370dcd3747c890
EikiKanaoka <kakakaeee@gmail.com>
2018-07-11 19:00:26 +0900

    Create Model Tag - word:string count:integer - db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710154639) do
+ActiveRecord::Schema.define(version: 20180711094739) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "word"
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
