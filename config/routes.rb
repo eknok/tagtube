76cd6f9a1c94a6fc0f38505769ef508d900edd93
EikiKanaoka <kakakaeee@gmail.com>
2018-07-11 19:09:28 +0900

    Generate TagController - only index, new, create, show - tags.scss in pages folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :tags, only: [:index, :new, :create, :show]
   devise_for :users, controller: { registrations: 'registrations' }
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
     get 'login',  to: 'devise/sessions#new'
   end
-  root to: 'toppages#index'
   get '/index', to: 'toppages#index'
   get '/about', to: 'toppages#about'
+  root to: 'toppages#index'
 end
